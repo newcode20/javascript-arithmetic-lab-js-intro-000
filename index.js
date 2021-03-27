756adc4dd44c4805054bcd9f9c73f6b7b920aa13
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-27 18:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function decrement(number){
 
 function makeInt(string){
   parseInt(string, 10);
-  return string;
+  return parseInt();
 }
 function preserveDecimal(string){
     parseFloat(string, 10);
